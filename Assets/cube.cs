57dc3ba9c030386dae7d4353afc8a2fa5d23b8d3
siyafinny <s161061@center.wakayama-u.ac.jp>
2015-12-06 23:35:50 +0900

    ログの表示

---
+++ Assets/cube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //cubeの動作を記述 
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
